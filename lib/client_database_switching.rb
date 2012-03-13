e00763154f21186bb80ff8d1ee35a4565fe4722a
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2012-03-13 22:42:27 +0000

    Support Rails 3

--- lib/client_database_switching.rb
+++ lib/client_database_switching.rb
@@ -14,12 +14,12 @@ module ClientDatabaseSwitching
     connect_to_database_for @@_client_database_details[host]
   end
 
-  # Looks in #{RAILS_ROOT}/config/databases for a configuration file which is called
+  # Looks in #{Rails.root.to_s}/config/databases for a configuration file which is called
   # #{client_hostname}.yml, which will be a ymlified Hash of the connection details
   # needed to establish a database connection.
   #
   def fetch_client_details_for(client_hostname)
-    file_path = "#{RAILS_ROOT}/config/databases/#{client_hostname}.yml"
+    file_path = "#{Rails.root.to_s}/config/databases/#{client_hostname}.yml"
     if File.exists?(file_path)
       return YAML::load(ERB.new(IO.read(file_path)).result)['database_details']
     end
